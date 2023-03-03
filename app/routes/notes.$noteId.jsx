850a851b3dc860ba8c86a489b451aab84f8a2b0e
rvira <rvira84@gmail.com>
2023-03-03 18:31:39 +0530

    added notes page

--- app/routes/notes.$noteId.jsx
+++ app/routes/notes.$noteId.jsx
@@ -1,18 +1,39 @@
 import {Link} from '@remix-run/react';
+import { json, useLoaderData } from 'react-router';
 import styles from '~/styles/note-details.css'
+import { getStoredNotes } from '../data/notes';
 
 export default function NoteDetailsPage(){
+
+    const note = useLoaderData();
+
+
     return(
         <main id="note-details">
             <header>
                 <nav>
                     <Link to="/notes"> Back to all notes </Link>
                 </nav>
-                <h1>NOTE TITLE</h1>
+                <h1>{note.title}</h1>
             </header>
-            <p id="note-details-content">NOTE CONTENT</p>
+            <p id="note-details-content">{note.content}</p>
         </main>
     );
+}
+
+export async function loader({params}){
+    const notes = await getStoredNotes();
+    const noteId = params.noteId;
+    const selectedNote = notes.find(note=> note.id === noteId);
+
+    if(!selectedNote){
+        throw json(
+            {message: 'Could not find note for Id '+ noteId},
+            {status: 404}
+        );
+    }
+
+    return selectedNote;
 }
 
 export function links(){
